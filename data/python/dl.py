5a0d051882fc42a396423a690ab0dd1a1e9ed532
Sawyer <928331892@qq.com>
2024-10-17 01:01:31 +0800

    Update dl.py

--- data/python/dl.py
+++ data/python/dl.py
@@ -29,31 +29,30 @@ except Exception as e:
 os.makedirs("./tmp/", exist_ok=True)
 
 # 复制补充规则到tmp文件夹
-subprocess.run("cp ./data/mod/adblock.txt ./tmp/adblock01.txt", shell=True)
-subprocess.run("cp ./data/mod/whitelist.txt ./tmp/allow01.txt", shell=True)
+#subprocess.run("cp ./data/mod/adblock.txt ./tmp/adblock01.txt", shell=True)
+#subprocess.run("cp ./data/mod/whitelist.txt ./tmp/allow01.txt", shell=True)
 
 
 # 拦截规则
 adblock = [
-    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_Base/filter.txt",
-    "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_224_Chinese/filter.txt",
-    "https://perflyst.github.io/PiHoleBlocklist/SmartTV-AGH.txt",
-    "https://easylist-downloads.adblockplus.org/easylist.txt",
-    "https://easylist-downloads.adblockplus.org/easylistchina.txt",
-    "https://easylist-downloads.adblockplus.org/easyprivacy.txt",
-    "https://raw.githubusercontent.com/Noyllopa/NoAppDownload/master/NoAppDownload.txt",
-    "https://raw.githubusercontent.com/sjhgvr/oisd/main/abp_small.txt",
-    "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt",
-    "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt",
-    "https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt"
+  "https://raw.githubusercontent.com/C9LG/DNS-Blocklists/main/rules/adblockdns.txt", #C9LG_DNS-Blocklists
+  "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_Base/filter.txt", #AdguardTeam_Base
+  "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_224_Chinese/filter.txt", #AdguardTeam_Chinese
+  "https://github.com/AdguardTeam/FiltersRegistry/raw/master/filters/filter_11_Mobile/filter.txt", #AdguardTeam_Mobile
+  "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt", #damengzhu_banad
+  "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt", #xinggsf_MV
+  "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_3_Spyware/filter.txt", #AdguardTeam_TrackingProtection
+  "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_14_Annoyances/filter.txt", #AdguardTeam_Annoyances
+  "https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt", #cjx82630_CJX-Annoyances
+  "https://secure.fanboy.co.nz/fanboy-annoyance.txt", #ryanbr_Fanboy-Annoyances
+  "https://raw.githubusercontent.com/Noyllopa/NoAppDownload/master/NoAppDownload.txt", #Noyllopa_NoAppDownload
+  "https://easylist.to/easylist/easyprivacy.txt", #EasyList_EasyPrivacy
+  "https://easylist-downloads.adblockplus.org/antiadblockfilters.txt", #EasyList_AdblockWarningRemovalList
+  "https://malware-filter.gitlab.io/malware-filter/phishing-filter-ag.txt" #malware-filter_Phishing URL Blocklist
 ]
 
 # 白名单规则
 allow = [
-    "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/ChineseFilter/sections/allowlist.txt",
-    "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/GermanFilter/sections/allowlist.txt",
-    "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/TurkishFilter/sections/allowlist.txt",
-    "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/SpywareFilter/sections/allowlist.txt"
 ]
 
 # 下载
@@ -66,5 +65,3 @@ for j, allow_url in enumerate(allow):
     time.sleep(1)
     
 print('规则下载完成')
-
-
